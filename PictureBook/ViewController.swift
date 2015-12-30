881e211700d6719131de3d7835bbe17e7e17f8c4
hunglun <hunglun.o@gmail.com>
2015-12-30 21:57:55 +0800

    v0.2 beautify the textfield font.

--- PictureBook/ViewController.swift
+++ PictureBook/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var textField: UITextField!
     @IBOutlet var webView: UIWebView!
+    let textAttributes = [
+        // black outline
+        NSStrokeColorAttributeName : UIColor(white: 0, alpha: 1),
+        NSForegroundColorAttributeName : UIColor(white: 0.5, alpha: 1),         // grey fill
+        NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName : -3
+    ]
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,8 +28,15 @@ class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate {
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        webView.delegate = self
         textField.delegate = self
+        textField.defaultTextAttributes = textAttributes
+        textField.textAlignment = .Center
+        textField.clearButtonMode = .WhileEditing
+
+        var frameRect = textField.frame;
+        frameRect.size.height +=  30
+        textField.frame = frameRect
+
     }
 
     
@@ -30,13 +45,17 @@ class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    func textFieldDidBeginEditing(textField: UITextField) {
+        textField.text = ""
+    }
+    
     func textFieldDidEndEditing(textField: UITextField) {
         print("Text Edit End")
         if let text = textField.text {
-            let url = NSURL (string: "https://www.google.com.sg/search?q=\(text)")
-            let requestObj = NSURLRequest(URL: url!)
-            webView.loadRequest(requestObj)
-            webView.frame.origin.y -= view.frame.height / 2.0
+            if let url = NSURL (string: "https://www.google.com.sg/search?q=\(text)") {
+                let requestObj = NSURLRequest(URL: url)
+                webView.loadRequest(requestObj)
+            }
         }
     }
 
