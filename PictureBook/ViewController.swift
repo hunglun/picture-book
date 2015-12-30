544233a51e085816c17c62a09c645f25108783b8
hunglun <hunglun.o@gmail.com>
2015-12-30 21:36:11 +0800

    v0.1 create a textfield and webview Enter event of textfield causes webview to request https://www.google.com.sg/search?q=\(text)")

--- PictureBook/ViewController.swift
+++ PictureBook/ViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate {
 
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        webView.delegate = self
+        textField.delegate = self
+    }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func textFieldDidEndEditing(textField: UITextField) {
+        print("Text Edit End")
+        if let text = textField.text {
+            let url = NSURL (string: "https://www.google.com.sg/search?q=\(text)")
+            let requestObj = NSURLRequest(URL: url!)
+            webView.loadRequest(requestObj)
+            webView.frame.origin.y -= view.frame.height / 2.0
+        }
+    }
+
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+
+        textField.resignFirstResponder()
+
+        return true
+    }
 
 }
 
